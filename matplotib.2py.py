03b71f6cf0c237b379e24f7ee1739389486f87b1
Hwangyeonjo <ddyungbbong@gmail.com>
2025-10-13 13:37:23 +0900

    delete 2

--- matplotib.2py.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import numpy as np
-import matplotlib.pyplot as plt
-
-y = [5, 3, 7, 10, 9, 5, 3.5, 8]
-x = range(len(y))
-plt.bar(x, y, width=0.7, color="blue")
-plt.show()
